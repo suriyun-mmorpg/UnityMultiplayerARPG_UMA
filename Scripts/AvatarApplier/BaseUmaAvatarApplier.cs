988960956dc0bfbcde5888a5a5ce25544ec8ebff
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-01-20 14:29:07 +0700

    Add game instance field for convenience

--- Scripts/AvatarApplier/BaseUmaAvatarApplier.cs
+++ Scripts/AvatarApplier/BaseUmaAvatarApplier.cs
@@ -5,7 +5,7 @@ namespace MultiplayerARPG
 {
     public abstract class BaseUmaAvatarApplier : ScriptableObject
     {
+        public GameInstance gameInstance { get { return GameInstance.Singleton; } }
         public abstract void Apply(DynamicCharacterAvatar umaAvatar, UmaAvatarData avatarData);
-        public abstract UmaAvatarData GetData(DynamicCharacterAvatar umaAvatar);
     }
 }
