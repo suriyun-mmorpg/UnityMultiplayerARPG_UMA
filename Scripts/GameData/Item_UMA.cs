6001875686ab9c3d0658ac5cd9c12a4acbf05170
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-01-23 00:57:15 +0700

    Add uma receipt support for item

---
+++ Scripts/GameData/Item_UMA.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UMA;
+
+namespace MultiplayerARPG
+{
+    public partial class Item
+    {
+        public UmaReceiptSlot[] umaReceiptSlots;
+
+        private Dictionary<string, UmaReceiptSlot> cacheUmaReceiptSlot;
+        private Dictionary<string, UmaReceiptSlot> CacheUmaReceiptSlot
+        {
+            get
+            {
+                if (cacheUmaReceiptSlot == null)
+                {
+                    cacheUmaReceiptSlot = new Dictionary<string, UmaReceiptSlot>();
+                    foreach (UmaReceiptSlot umaReceiptSlot in umaReceiptSlots)
+                    {
+                        if (umaReceiptSlot.raceData == null || string.IsNullOrEmpty(umaReceiptSlot.raceData.raceName))
+                            continue;
+                        cacheUmaReceiptSlot.Add(umaReceiptSlot.raceData.raceName, umaReceiptSlot);
+                    }
+                }
+                return cacheUmaReceiptSlot;
+            }
+        }
+    }
+
+    [System.Serializable]
+    public struct UmaReceiptSlot
+    {
+        public RaceData raceData;
+        public UMATextRecipe recipe;
+        public string slot;
+    }
+
+}
