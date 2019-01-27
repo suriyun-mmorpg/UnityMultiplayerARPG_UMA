240c7bc3ca5e866b0f42bef67cb9a3d544e7c5a4
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-01-28 04:28:23 +0700

    WIP apply extra

---
+++ Scripts/MMO/Networking/CentralNetworkManager_UMA.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG.MMO
+{
+    public partial class CentralNetworkManager
+    {
+        [DevExtMethods("ApplyCreateCharacterExtra")]
+        public void ApplyCreateCharacterExtra_UMA(PlayerCharacterData characterData, byte[] extra)
+        {
+            UmaAvatarData umaAvatarData = new UmaAvatarData();
+            umaAvatarData.SetBytes(extra);
+            characterData.UmaAvatarData = umaAvatarData;
+        }
+    }
+}
