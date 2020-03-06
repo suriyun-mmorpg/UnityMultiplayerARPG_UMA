894583cb605e92864c8ba655e9c24d3f4b58f244
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-03-07 04:58:49 +0700

    Show dialog after conversion

--- Scripts/CharacterModelUMA.cs
+++ Scripts/CharacterModelUMA.cs
@@ -371,7 +371,7 @@ namespace MultiplayerARPG
             model.effectContainers = effectContainers;
             model.equipmentContainers = equipmentContainers;
             EditorUtility.SetDirty(model);
-            Debug.Log("[Character Model UMA] Converted, you can remove this component.");
+            EditorUtility.DisplayDialog("Character Model Conversion", "New Character Model component has been added.\n\nThe old component doesn't removed yet to let you check values.\n\nThen, you have to remove the old one.", "OK");
         }
 #endif
     }
