24ff9ac72c65fa4eec3ec9aeb0ac18a69202de6b
Insthync <ittipon.bay@gmail.com>
2021-01-28 16:15:14 +0700

    Fix wrong partial class

--- Scripts/CharacterData/PlayerCharacterDataExtension_UMA.cs
+++ Scripts/CharacterData/PlayerCharacterDataExtension_UMA.cs
@@ -1,27 +1,27 @@
 ﻿using LiteNetLib.Utils;
-using System.Collections;
-using System.Collections.Generic;
-using MultiplayerARPG;
 
-public partial class PlayerCharacterDataExtension
+namespace MultiplayerARPG
 {
-    [DevExtMethods("CloneTo")]
-    public static void CloneTo_UMA(IPlayerCharacterData from, IPlayerCharacterData to)
+    public partial class PlayerCharacterDataExtension
     {
-        to.UmaAvatarData = from.UmaAvatarData;
-    }
+        [DevExtMethods("CloneTo")]
+        public static void CloneTo_UMA(IPlayerCharacterData from, IPlayerCharacterData to)
+        {
+            to.UmaAvatarData = from.UmaAvatarData;
+        }
 
-    [DevExtMethods("SerializeCharacterData")]
-    public static void SerializeCharacterData_UMA(IPlayerCharacterData characterData, NetDataWriter writer)
-    {
-        characterData.UmaAvatarData.Serialize(writer);
-    }
+        [DevExtMethods("SerializeCharacterData")]
+        public static void SerializeCharacterData_UMA(IPlayerCharacterData characterData, NetDataWriter writer)
+        {
+            characterData.UmaAvatarData.Serialize(writer);
+        }
 
-    [DevExtMethods("DeserializeCharacterData")]
-    public static void DeserializeCharacterData_UMA(IPlayerCharacterData characterData, NetDataReader reader)
-    {
-        UmaAvatarData umaAvatarData = new UmaAvatarData();
-        umaAvatarData.Deserialize(reader);
-        characterData.UmaAvatarData = umaAvatarData;
+        [DevExtMethods("DeserializeCharacterData")]
+        public static void DeserializeCharacterData_UMA(IPlayerCharacterData characterData, NetDataReader reader)
+        {
+            UmaAvatarData umaAvatarData = new UmaAvatarData();
+            umaAvatarData.Deserialize(reader);
+            characterData.UmaAvatarData = umaAvatarData;
+        }
     }
 }
